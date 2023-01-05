da9bdd8b9af462ccf0d7723cbc74d9baebbcf364
AnasAlRimawi <20195840@ug.buid.ac.ae>
2023-01-05 23:11:26 +0400

    Initial commit

---
+++ web.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from main import*
+
+st.set_page_config(page_title='RS3 Grand Exchange', layout='centered')
+
+st.title("RS3 Grand Exchange")
+
+items = itemList.keys()
+selectedItem = st.selectbox("Search an item", options=items)
+
+button2 = st.button("Look Up Price")
+if button2:
+    number_with_commas = "{:,}".format(fetchPrice(selectedItem))
+    st.markdown("Current price for "+ selectedItem +" is "+ number_with_commas +" GP" + ":moneybag:")
